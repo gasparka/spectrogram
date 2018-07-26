136ce478faf22114c20bc95f966675d3ae13006e
Gaspar Karm <gkarm@live.com>
2018-07-26 13:24:33 +0300

    main: clear fft buffer if GUI is very slow, else program may get into infinite loop

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import signal
 import sys
 import time
@@ -9,8 +10,12 @@ from fft_reader import FFTReader
 from util import rescale_intensity
 import pickle
 
-PACKETS = 2000
-PACKETS_PER = PACKETS // 20
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger('main')
+
+SCREEN_FFTS = 2000
+SCREEN_FFTS_PACKETS = 20
+PACKET_SIZE = SCREEN_FFTS // SCREEN_FFTS_PACKETS
 
 
 class SpectrogramWidget(pg.PlotWidget):
@@ -34,7 +39,7 @@ class SpectrogramWidget(pg.PlotWidget):
         self.last_call = time.time()
         self.show()
 
-        self.img_array = np.zeros(shape=(PACKETS, 512))
+        self.img_array = np.zeros(shape=(SCREEN_FFTS, 512))
 
     # @profile
     def main(self):
@@ -43,9 +48,17 @@ class SpectrogramWidget(pg.PlotWidget):
         while FFTReader.output_queue.empty():
             pass
 
+        i = 0
         while not FFTReader.output_queue.empty():
             fft = FFTReader.output_queue.get()
-            self.img_array = np.vstack([self.img_array[PACKETS_PER:], fft])
+            self.img_array = np.vstack([self.img_array[PACKET_SIZE:], fft])
+            i += 1
+            if i >= SCREEN_FFTS_PACKETS:
+                # have already loaded full screen...
+                log.warning('Plotting is slower than FFT interface...throwing away excess buffer..')
+                while not FFTReader.output_queue.empty():
+                    FFTReader.output_queue.get()
+                break
 
         p2, p98 = np.percentile(self.img_array, (2, 98))
         ret = rescale_intensity(self.img_array, in_range=(p2, p98))
@@ -53,15 +66,12 @@ class SpectrogramWidget(pg.PlotWidget):
         self.img.setImage(ret, autoLevels=False)
 
 
-if __name__ == '__main__':
+def init():
     signal.signal(signal.SIGINT, signal.SIG_DFL)  # ctrl-c can kill QT event loop!
     app = QApplication(sys.argv)
 
-    fft_reader = FFTReader(PACKETS_PER)
+    fft_reader = FFTReader(PACKET_SIZE)
     fft_reader.start()
-
-    # image_maker = ImageMaker()
-    # image_maker.start()
 
     w = SpectrogramWidget()
 
@@ -73,5 +83,6 @@ if __name__ == '__main__':
     fft_reader.alive.value = False
     fft_reader.join()
 
-    # image_maker.alive.value = False
-    # fft_reader.join()
+
+if __name__ == '__main__':
+    init()
