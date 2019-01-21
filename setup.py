00580ea83ee356b164dbd5c997c8ca538c20a441
Gaspar Karm <gkarm@live.com>
2019-01-21 14:54:41 +0200

    pip installable, setups 'spectrogram' cli command

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='spectrogram',
+    version='0.0.1',
+    description="Spectrogram (80MHz bandwidth) accelerator for LimeSDR",
+    long_description=readme,
+    author="Gaspar Karm",
+    author_email='gkarm@live.com',
+    url='https://github.com/gasparka/realtime_spectrogram',
+    license="Apache Software License 2.0",
+    keywords='spectrogram, fft, limesdr',
+    scripts=['bin/spectrogram']
+)
