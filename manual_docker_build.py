0877904927ae4d889401800f852ec1e5f221380c
Gaspar Karm <gkarm@live.com>
2019-01-23 18:56:49 +0200

    docker: add script to manually build and push images

---
+++ manual_docker_build.py
@@ -0,0 +1,11 @@
+import subprocess
+
+
+# no_cache = '--no-cache'
+no_cache = ''
+
+subprocess.run(f'docker build {no_cache} -t gasparka/spectrogram .', shell=True)
+subprocess.run(f'docker push gasparka/spectrogram', shell=True)
+
+subprocess.run(f'docker build {no_cache} -t gasparka/spectrogram:arm -f Dockerfile.arm .', shell=True)
+subprocess.run(f'docker push gasparka/spectrogram:arm', shell=True)
