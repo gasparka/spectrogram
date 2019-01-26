2f5e4bb62a7fc2fa52708b21d30c41613321c97e
Gaspar Karm <gkarm@live.com>
2019-01-26 19:30:57 +0200

    Bump version: 1.0.2 → 1.0.3

--- release.py
+++ release.py
@@ -1,10 +1,10 @@
 import subprocess
 
 # submodules need to be updated, make sure i dont forget
-subprocess.run('git add -a', shell=True)
+subprocess.run('git add --all', shell=True)
 
 # new version, this makes a git commit and a TAG (triggers docker hub and travisCI builds)
-# subprocess.run('bumpversion --allow-dirty --tag patch', shell=True)
+subprocess.run('bumpversion --allow-dirty --tag patch', shell=True)
 
 # TODO: need to set all repos to SSH to use this?
 # subprocess.run('git push', shell=True)
