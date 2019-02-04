0d3272068eef4b852c0ad834ef4ccdd48455ca5e
Gaspar Karm <gkarm@live.com>
2019-02-04 14:18:01 +0200

    docker: only one push needed

--- manual_docker_build.py
+++ manual_docker_build.py
@@ -5,7 +5,5 @@ import subprocess
 no_cache = ''
 
 subprocess.run(f'docker build {no_cache} -t gasparka/spectrogram .', shell=True)
-subprocess.run(f'docker push gasparka/spectrogram', shell=True)
-
 subprocess.run(f'docker build {no_cache} -t gasparka/spectrogram:arm -f Dockerfile.arm .', shell=True)
-subprocess.run(f'docker push gasparka/spectrogram:arm', shell=True)
+subprocess.run(f'docker push gasparka/spectrogram', shell=True)
