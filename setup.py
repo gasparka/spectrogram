1fa43fca7a5d59d928245059369b68e4d12bfdfd
Gaspar Karm <gkarm@live.com>
2019-02-05 15:29:27 +0200

    Bump version: 1.0.5 → 1.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme_file:
 
 setup(
     name='spectrogram',
-    version='1.0.5',
+    version='1.0.6',
     python_requires='>3.4',
     description="Spectrogram (80MHz bandwidth) accelerator for LimeSDR",
     long_description=readme,
